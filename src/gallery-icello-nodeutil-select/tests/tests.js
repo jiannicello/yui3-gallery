0ef4dd229602555ca22332900a921c99efa8d5c9
John Iannicello <johnicello@gmail.com>
2012-03-19 22:20:00 -0400

    start of icello.Select to widget with first failed test

--- src/gallery-icello-nodeutil-select/tests/tests.js
+++ src/gallery-icello-nodeutil-select/tests/tests.js
@@ -3,9 +3,10 @@ YUI().use('gallery-icello-nodeutil-select', 'gallery-icello-qunit-step', 'node-e
     
     Y.on('domready', function (e) {
         Y.log('', 'info', 'domready');
-        var ddl = Y.one('#ddl');
+        
         
         module('select tests');
+        /*
         test('getSelectedValue should return "mets"', 1, function () {
             var selectedValue = 'mets';
             Select.append(ddl, 'Please Select', selectedValue);
@@ -27,6 +28,30 @@ YUI().use('gallery-icello-nodeutil-select', 'gallery-icello-qunit-step', 'node-e
             equal(option.getContent(), 'Mets', '"getSelectedOption(ddl).getContent()" should equal "Mets"');
 
             ddl.empty();
+        });
+        */
+        test('render with 3 "items" should result in 3 option nodes', 5, function () {
+            var option = null,
+                ddl = new Select({
+                srcNode:'#ddl',
+                items: [
+                    {text:'Yankees', value:'yanks'},
+                    {text:'Mets', value:'mets', selected:true},
+                    {text:'RedSox', value:'redsox'}
+                ]
+            });
+            
+            equal(ddl.sizeSelected(), 0, 'sizeSelected() should return 0 before rendering');
+            
+            step(1, 'step 1: render and set "option" with "getOptionSelected() call');
+            ddl.render();
+            option = ddl.getOptionSelected();
+            
+            equal(ddl.sizeSelected(), 1, 'sizeSelected() should return 1 after rendering');
+            equal(option.getContent(), 'Mets', 'option.getContent() should return "Mets"');
+            equal(option.get('value'), 'mets', 'option.get("value") should return "mets"');
+            
+            ddl.destroy();
         });
     });
 });
