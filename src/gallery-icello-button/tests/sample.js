dab9eb2f49d076ae7805af06f727a3889aeff1f6
John Iannicello <johnicello@gmail.com>
2012-03-31 16:20:55 -0400

    updated Button sample to not use tabs

--- src/gallery-icello-button/tests/sample.js
+++ src/gallery-icello-button/tests/sample.js
@@ -5,24 +5,23 @@ YUI().use('gallery-icello-button', function (Y) {
                 icon: Button.ICONS.ALERT,
                 label: 'ALERT Button'
             });
-            
-            btn.render();
-			
-			btn.on('click', function () {
-				//e.preventDefault() is not needed'
-				var curr_icon = btn.get('icon');
-				
-				if(curr_icon === Button.ICONS.ALERT) {
-					btn.set('icon', Button.ICONS.INFO);
-					btn.set('label', 'INFO Button');
-				} else {
-					btn.set('icon', Button.ICONS.ALERT);
-					btn.set('label', 'ALERT Button');
-				}
-				
-				//calling btn.syncUI() is needed, there are no 'after' handlers for icon and label, because css is different depending on whether it's label/icon, label only or icon only"
-				btn.syncUI();
-			});
-			
+        
+        btn.on('click', function () {
+            //e.preventDefault() is not needed'
+            var curr_icon = btn.get('icon');
+
+            if(curr_icon === Button.ICONS.ALERT) {
+                btn.set('icon', Button.ICONS.INFO);
+                btn.set('label', 'INFO Button');
+            } else {
+                btn.set('icon', Button.ICONS.ALERT);
+                btn.set('label', 'ALERT Button');
+            }
+
+            //calling btn.syncUI() is needed, there are no 'after' handlers for icon and label, because css is different depending on whether it's label/icon, label only or icon only"
+            btn.syncUI();
+        });
+
+        btn.render();	
     });
 });
